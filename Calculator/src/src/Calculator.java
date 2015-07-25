e16f7703b01dfd61443be823d79ba72f68d32a96
ymatlashenko <mjana@i.ua>
2015-07-25 13:34:43 +0300

    Calc

--- Calculator/src/src/Calculator.java
+++ Calculator/src/src/Calculator.java
@@ -1,130 +1,214 @@
 package src;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.FlowLayout;
 import javax.swing.*;
- 
-public class Calculator
-{
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run() {
-				CalculatorFrame frame = new CalculatorFrame();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-			}
-		});
-	}
-}
-class CalculatorFrame extends JFrame {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-	public CalculatorFrame() {
-		setTitle("Calculator");
-		CalculatorPanel panel = new CalculatorPanel();
-		add(panel);
-		pack();
+public class Calculator implements ActionListener {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JTextArea text;
+    private JButton but1, but2, but3, but4, but5, but6, but7, but8, but9, but0, butAdd, butMinus, butMultiply, butDivide, butEqual, butCancel;
+    private int num1, num2;
+	int result;
+    private int add = 0, minus = 0, multiply = 0, divide = 0;
+    
+	public Calculator() {
+		frame = new JFrame("Calculator");
+		panel = new JPanel(new FlowLayout());
+		
+		text = new JTextArea(1,20);
+		but1 = new JButton("1");
+	    but2 = new JButton("2");
+		but3 = new JButton("3");
+		but4 = new JButton("4");
+		but5 = new JButton("5");
+		but6 = new JButton("6");
+	    but7 = new JButton("7");
+		but8 = new JButton("8");
+		but9 = new JButton("9");
+		but0 = new JButton("0");
+		
+		butAdd = new JButton("+");
+		butMinus = new JButton("-");
+		butMultiply = new JButton("*");
+		butDivide = new JButton("/");
+		butEqual = new JButton("=");
+		
+		butCancel = new JButton("C");
+		
 	}
-}
-class CalculatorPanel extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public CalculatorPanel() {
-		setLayout(new BorderLayout());
- 
-		result = 0;
-		lastCommand = "=";
-		start=true;
- 
-		display = new JButton("0");
-		display.setEnabled(false);
-		add(display, BorderLayout.NORTH);
- 
-		ActionListener insert = new InsertAction();
-		ActionListener command = new CommandAction();
- 
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(4, 4));
- 
-		addButton("7", insert);
-		addButton("8", insert);
-		addButton("9", insert);
-		addButton("/", command);
- 
-		addButton("4", insert);
-		addButton("5", insert);
-		addButton("6", insert);
-		addButton("*", command);
- 
-		addButton("1", insert);
-		addButton("2", insert);
-		addButton("3", insert);
-		addButton("-", command);
- 
-		addButton("0", insert);
-		addButton(".", insert);
-		addButton("=", command);
-		addButton("+", command);
- 
-		add(panel, BorderLayout.CENTER);
+	
+	public void init() {
+		frame.setVisible(true);
+		frame.setSize(250,250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.add(text);
+		panel.add(but1);
+		panel.add(but2);
+		panel.add(but3);
+		panel.add(but4);
+		panel.add(but5);
+		panel.add(but6);
+		panel.add(but7);
+		panel.add(but8);
+		panel.add(but9);
+		panel.add(but0);
+		
+		panel.add(butAdd);
+		panel.add(butMinus);
+		panel.add(butMultiply);
+		panel.add(butDivide);
+		panel.add(butEqual);
+		
+		panel.add(butCancel);
+		
+		but1.addActionListener(this);
+		but2.addActionListener(this);
+		but3.addActionListener(this);
+		but4.addActionListener(this);
+		but5.addActionListener(this);
+		but6.addActionListener(this);
+		but7.addActionListener(this);
+		but8.addActionListener(this);
+		but9.addActionListener(this);
+		but0.addActionListener(this);
+		
+		butAdd.addActionListener(this);
+		butMinus.addActionListener(this);
+		butMultiply.addActionListener(this);
+		butDivide.addActionListener(this);
+		butEqual.addActionListener(this);
+		
+		butCancel.addActionListener(this);
+
 	}
-	private void addButton(String label, ActionListener listener) {
-		JButton button = new JButton(label);
-		button.addActionListener(listener);
-		panel.add(button);
-	}
-	private class InsertAction implements ActionListener 
-	{
-		public void actionPerformed(ActionEvent event) 
-		{
-			String input = event.getActionCommand();
-			if(start) {
-				display.setText("");
-				start = false;
-			}
-			display.setText(display.getText() + input);
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		
+		if(source == but1) {
+			text.append("1");
 		}
-	}
-	private class CommandAction implements ActionListener 
-	{
-		public void actionPerformed(ActionEvent event) 
-		{
-			String command = event.getActionCommand();
-			if(start) 
-			{
-				if(command.equals("-")) 
-				{
-					display.setText(command);
-					start = false;
-				}
-				else lastCommand = command;
+		
+		if(source == but2) {
+			text.append("2");
+		}
+
+		if(source == but3) {
+			text.append("3");
+		}
+		
+		if(source == but4) {
+			text.append("4");
+		}
+		
+		if(source == but5) {
+			text.append("5");
+		}
+		
+		if(source == but6) {
+			text.append("6");
+		}
+		
+		if(source == but7) {
+			text.append("7");
+		}
+		
+		if(source == but8) {
+			text.append("8");
+		}
+		
+		if(source == but9) {
+			text.append("9");
+		}
+		
+		if(source == but0) {
+			text.append("0");
+		}
+			
+		if(source == butAdd) {
+			num1 = reader();
+			text.setText("");
+			add = 1;
+			minus = 0;
+			multiply = 0;
+			divide = 0;
+		}
+		
+		if(source == butMinus) {
+			num1 = reader();
+			text.setText("");
+			add = 0;
+			minus = 1;
+			multiply = 0;
+			divide = 0;
+		}
+		
+		if(source == butMultiply) {
+			num1 = reader();
+			text.setText("");
+			add = 0;
+			minus = 0;
+			multiply = 1;
+			divide = 0;
+		}
+		
+		if(source == butDivide) {
+			num1 = reader();
+			text.setText("");
+			add = 1;
+			minus = 0;
+			multiply = 0;
+			divide = 1;
+		}
+		
+		if(source == butEqual) {
+			num2 = reader();
+			
+			if(add > 0) {
+				result = (int) (num1+num2);
+				text.setText(Integer.toString(result));
 			}
-			else 
-			{
-				calculate(Double.parseDouble(display.getText()));
-				lastCommand = command;
-				start=true;
+			
+			if(minus > 0) {
+				result = (int) (num1-num2);
+				text.setText(Integer.toString(result));
 			}
-		}	
+			
+			if(multiply > 0) {
+				result = (int) (num1*num2);
+				text.setText(Integer.toString(result));
+			}
+			
+			if(divide > 0) {
+				result = (int) (num1/num2);
+				text.setText(Integer.toString(result));
+			}
+			
+		}
+		
+		if(source == butCancel) {
+			num1 = (int) 0;
+			num2 = (int) 0;
+			text.setText("");
+		}
+	
 	}
-	public void calculate(double x)
-	{
-		if(lastCommand.equals("+")) result += x;
-		else if(lastCommand.equals("-")) result -= x;
-		else if(lastCommand.equals("*")) result *= x;
-		else if(lastCommand.equals("/")) result /= x;
-		else if(lastCommand.equals("=")) result = x;
-		display.setText("" + result);
+	
+	public int reader() {
+		int num;
+		String str;
+		str = text.getText();
+		num = Integer.valueOf(str);
+		
+		return num;	
 	}
-	private JButton display;
-	private JPanel panel;
-	private double result;
-	private String lastCommand;
-	private boolean start;
+	
+
 }
---
+++ Calculator/src/src/MainCalc.java
@@ -0,0 +1,11 @@
+package src;
+
+public class MainCalc {
+	public static void main(String[] args) {
+		Calculator uiCal = new Calculator();
+		uiCal.init();
+	}
+
+}
+
+
